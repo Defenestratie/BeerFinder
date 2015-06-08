6e7d7430b85b7b98935230ede7e2328e6eecc597
HansHoogerwerf <hans3132@live.nl>
2015-06-08 13:18:40 +0200

    Settings

--- BeerFinder/app/src/main/java/com/beerfinder/beerfinder/MapActivity.java
+++ BeerFinder/app/src/main/java/com/beerfinder/beerfinder/MapActivity.java
@@ -1,12 +1,14 @@
 package com.beerfinder.beerfinder;
 
 import android.content.Context;
+import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,6 +30,21 @@ public class MapActivity extends FragmentActivity {
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(getApplicationContext(), UserPreferences.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onResume() {
