8a5b625f6ccc1dda23d6b20ef1e5615e1da527e5
HansHoogerwerf <hans3132@live.nl>
2015-06-10 10:49:32 +0200

    getting user settings

--- BeerFinder/app/src/main/java/com/beerfinder/beerfinder/MainActivity.java
+++ BeerFinder/app/src/main/java/com/beerfinder/beerfinder/MainActivity.java
@@ -1,15 +1,24 @@
 package com.beerfinder.beerfinder;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 
 public class MainActivity extends Activity {
@@ -43,15 +52,35 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public void openMap(View v)
     {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
         String radius = sp.getString("beerRadius","");
 
+        switch(radius)
+        {
+            case "1" : radius = "1";break;
+            case "2" : radius = "2";break;
+            case "3" : radius = "5";break;
+            case "4" : radius = "10";break;
+            case "5" : radius = "25";break;
+
+        }
+
+        Set<String> set = new HashSet<String>();
+
+        set = sp.getStringSet("beerBrands", set);
+
+        for (String s : set) {
+            Log.d("beerBrands", s);
+        }
 
 
-        Toast.makeText(getApplicationContext(),"Radius " + radius, Toast.LENGTH_SHORT);
+
+
+        //Toast.makeText(getApplicationContext(),"Radius " + radius, Toast.LENGTH_SHORT);
 
 
 //        Intent map =  new Intent(this, MapActivity.class);
