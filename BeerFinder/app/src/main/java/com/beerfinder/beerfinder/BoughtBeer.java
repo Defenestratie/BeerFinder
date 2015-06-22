b909548a954804ca33fca88988c0bb5b3ade3c3a
fnvherk <0898132@hr.nl>
2015-06-22 17:29:49 +0200

    Activity + layout voor  bier op database zetten gemaakt

---
+++ BeerFinder/app/src/main/java/com/beerfinder/beerfinder/BoughtBeer.java
@@ -0,0 +1,18 @@
+package com.beerfinder.beerfinder;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class BoughtBeer extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bought_beer);
+    }
+
+
+}
