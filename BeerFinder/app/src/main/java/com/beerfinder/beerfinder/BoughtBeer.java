04b172667aa002a7d86bf3fc3ae84bed346ea1c1
fnvherk <0898132@hr.nl>
2015-06-22 23:54:17 +0200

    Activity + layout bewerkt. Staat nu arraylist in. Verbinding met database moet nog gebeuren.

--- BeerFinder/app/src/main/java/com/beerfinder/beerfinder/BoughtBeer.java
+++ BeerFinder/app/src/main/java/com/beerfinder/beerfinder/BoughtBeer.java
@@ -5,14 +5,34 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class BoughtBeer extends Activity {
+    ListView beersAtLocationListView;
+    ArrayList<String> boughtBeerList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bought_beer);
+
+        boughtBeerList = new ArrayList<>();
+        ArrayAdapter boughtBeerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, boughtBeerList);
+
+        beersAtLocationListView = (ListView)findViewById(R.id.beersAtLocationList);
+        beersAtLocationListView.setAdapter(boughtBeerAdapter);
     }
 
+    public void addBeer(View v){
+        Button button = (Button)v;
 
+        //TODO add to database
+    }
 }
