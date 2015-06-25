dc687ef0f990549402fea4929dde97e438a3260a
fnvherk <0898132@hr.nl>
2015-06-25 18:03:09 +0200

    Voorkeuren gebruiker wordt opgehaald, moet alleen nog worden gebruikt. App kan niet meer kantelen.

--- BeerFinder/app/src/main/java/com/beerfinder/beerfinder/UserPreferences.java
+++ BeerFinder/app/src/main/java/com/beerfinder/beerfinder/UserPreferences.java
@@ -3,19 +3,38 @@ package com.beerfinder.beerfinder;
 import android.annotation.TargetApi;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Set;
 
 /**
  * Created by Florian on 8-6-2015.
  */
 public class UserPreferences extends PreferenceActivity {
+    SharedPreferences settings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.user_preferences);
+
+        settings = PreferenceManager.getDefaultSharedPreferences(UserPreferences.this);
+        String radius = settings.getString("beerRadius", null);
+        Set<String> beerBrands = settings.getStringSet("beerBrands", null);
+        Set<String> beerTypes = settings.getStringSet("beerTypes", null);
+        Set<String> establishments = settings.getStringSet("establishments", null);
+        //TODO in JSON zetten
+
+        Log.d("Tag", "Radius: " + radius);
+        Log.d("Tag", "Brands: " + beerBrands);
+        Log.d("Tag", "Types: " + beerTypes);
+        Log.d("Tag", "Establishments: " + establishments);
+
     }
 }
